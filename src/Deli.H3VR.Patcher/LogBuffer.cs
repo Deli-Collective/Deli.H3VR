c895872f3eb381961aee85c1ff9b607b3ee49b26
Nathan Gill <nrgill28@gmail.com>
2021-04-28 02:07:07 -0400

    Patching stage log buffer to capture logs earlier than a behaviour can.

---
+++ src/Deli.H3VR.Patcher/LogBuffer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using BepInEx.Logging;
+using Deli.Patcher;
+
+namespace Deli.H3VR.Patcher
+{
+	internal class LogBuffer : DeliModule, ILogListener
+	{
+		// We'll need a reference to this from the behaviour
+		internal static LogBuffer? Instance { get; private set; }
+
+		// Add ourselves as a listener for log events
+		public LogBuffer(Mod source) : base(source)
+		{
+			Instance = this;
+			BepInEx.Logging.Logger.Listeners.Add(this);
+		}
+
+		// When the Deli behaviour takes over we can stop listening for logs
+		public void Dispose()
+		{
+			Instance = null;
+			BepInEx.Logging.Logger.Listeners.Remove(this);
+		}
+
+		// Capture log events
+		internal List<LogEventArgs> LogEvents = new();
+		public void LogEvent(object sender, LogEventArgs eventArgs) => LogEvents.Add(eventArgs);
+
+	}
+}
--- src/Deli.H3VR/src/LogPanel/BepInExLogPanel.cs
+++ src/Deli.H3VR/src/LogPanel/BepInExLogPanel.cs
@@ -1,13 +1,34 @@
-﻿using Deli.H3VR.Api;
+﻿using System;
+using System.Collections.Generic;
+using BepInEx.Logging;
+using Deli.H3VR.Api;
+using Deli.H3VR.Patcher;
 using Deli.Setup;
 
 namespace Deli.H3VR.LogPanel
 {
-	public class BepInExLogPanel : DeliBehaviour
+	public class BepInExLogPanel : DeliBehaviour, ILogListener
 	{
+		private List<LogEventArgs> _logEvents;
+
 		public BepInExLogPanel()
 		{
 			Stages.Setup += OnSetup;
+
+			// Register ourselves as the new log listener and try to grab what's already been captured
+			BepInEx.Logging.Logger.Listeners.Add(this);
+			if (LogBuffer.Instance is not null)
+			{
+				// Grab the captured logs from the buffer and dispose it.
+				_logEvents = LogBuffer.Instance.LogEvents;
+				LogBuffer.Instance.Dispose();
+			}
+			else
+			{
+				// If the instance was somehow null we can still continue, just without the previous logs.
+				_logEvents = new List<LogEventArgs>();
+				Logger.LogError("LogBuffer instance was null! Captured logs will start from here instead.");
+			}
 		}
 
 		private void OnSetup(SetupStage stage)
@@ -16,6 +37,17 @@ namespace Deli.H3VR.LogPanel
 			{
 				Logger.LogInfo("Maybe open the log panel or something idk");
 			});
+		}
+
+		void ILogListener.LogEvent(object sender, LogEventArgs eventArgs)
+		{
+			_logEvents.Add(eventArgs);
+		}
+
+		void IDisposable.Dispose()
+		{
+			BepInEx.Logging.Logger.Listeners.Remove(this);
+			_logEvents.Clear();
 		}
 	}
 }
